7b4f901f3d6d109867f34ac0a3ac32b1745dac34
temi134 <temidee2012@gmail.com>
2025-11-17 17:58:20 +0000

    Add files via upload

--- Python maze.py
+++ Python maze.py
@@ -8,17 +8,17 @@ pen.speed(100)
 
 maze = [
     "XXXXXXXXXXXXXXX",
-    "X   X         X",
+    "X   X       C X",
     "X XXXXX XXXXX X",
-    "X X     X     X",
+    "X X   C  X    X",
     "X X XXX X XXX X",
     "X X   X X X   X",
     "X XXX X X X XXX",
-    "X     X X X   X",
+    "X   C X X X   X",
     "XXXXX X X X XXX",
-    "X     X   X   X",
+    "X  C  X   X   X",
     "X XXXXX XXXXX X",
-    "X             X",
+    "X   C         X",
     "XXXXXXXXXXXXX F"]
 
 player = turtle.Turtle()
@@ -29,6 +29,9 @@ player.penup()
 
 
 obstacles = []
+coins =[]
+finish_line = turtle.Turtle()
+counter = 0
 
 def create_maze():
     global finish_line
@@ -41,13 +44,21 @@ def create_maze():
             if character == 'X':
                 obstacle = turtle.Turtle()
                 obstacle.shape('square')
-                obstacle.color('yellow')
+                obstacle.color('brown')
                 obstacle.penup()
                 obstacle.goto(screen_x,screen_y)
                 obstacles.append(obstacle)
 
+            if character == 'C':
+                coin = turtle.Turtle()
+                coin.shape('circle')
+                coin.color('yellow')
+                coin.penup()
+                coin.goto(screen_x,screen_y)
+                coins.append(coin)
+
+
             elif character == 'F':
-                finish_line = turtle.Turtle()
                 finish_line.shape('circle')
                 finish_line.color('black')
                 finish_line.penup()
@@ -59,6 +70,7 @@ def move_up():
     if is_valid_move(new_x,new_y):
         player.goto(new_x,new_y)
         check_win()
+        is_coin(new_x,new_y)
 
 def move_down():
     new_x = player.xcor()
@@ -66,6 +78,7 @@ def move_down():
     if is_valid_move(new_x,new_y):
         player.goto(new_x,new_y)
         check_win()
+        is_coin(new_x,new_y)
 
 def move_left():
     new_x = player.xcor() - 24
@@ -73,6 +86,7 @@ def move_left():
     if is_valid_move(new_x,new_y):
         player.goto(new_x,new_y)
         check_win()
+        is_coin(new_x,new_y)
 
 def move_right():
     new_x = player.xcor() + 24
@@ -80,25 +94,40 @@ def move_right():
     if is_valid_move(new_x,new_y):
         player.goto(new_x,new_y)
         check_win()
-
-paper.listen()
-paper.onkey(move_up,'Up')
-paper.onkey(move_down,'Down')
-paper.onkey(move_left,'Left')
-paper.onkey(move_right,'Right')
-
+        is_coin(new_x,new_y)
 def is_valid_move(x,y):
     for obstacle in obstacles:
         if obstacle.xcor() == x and obstacle.ycor() == y:
             return False
     return True
 
+def is_coin(x,y): 
+    global counter
+    for coin in coins:
+        if coin.xcor() == x and coin.ycor() == y:
+            coin.hideturtle()
+            counter = counter+1
+            print(counter)
+
+
+
+
+
+        
+
 def check_win():
-    if player.distance(finish_line) < 12:
+    if player.distance(finish_line) < 12 and counter == 5:
         player.hideturtle()
         finish_line.hideturtle()
-        paper.bye()
-        print('Congratulations you have completed the maze')
+        turtle.up()
+        turtle.goto(-280,-20)
+        turtle.write('Congratulations you have completed the maze',font=('Arial',15,'bold'))
+paper.listen()
+paper.onkey(move_up,'Up')
+paper.onkey(move_down,'Down')
+paper.onkey(move_left,'Left')
+paper.onkey(move_right,'Right')
+
 
 create_maze()
 player.goto(-264,+264)
