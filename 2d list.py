552362923f044685db6e454807d92f58044f230c
temi134 <temidee2012@gmail.com>
2025-12-08 17:52:58 +0000

    Add files via upload

---
+++ 2d list.py
@@ -0,0 +1,12 @@
+number = [[1,2,4,5,],
+          [4,5,6,12],
+          [7,8,9,10]]
+
+'''for row in number:
+    for num in row:
+        print(num,end= ' ')
+    print()'''
+for i in range(0,3):
+    for j in range(0,3):
+        print(number[i][j],end = ' ') 
+    print()
