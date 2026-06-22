462d1f0dfc2007ef0306564947e1a51218e77d6b
temi134 <temidee2012@gmail.com>
2026-06-22 17:33:15 +0100

    Add files via upload

--- bday_balloon.py
+++ bday_balloon.py
@@ -10,19 +10,25 @@ font1 = pygame.font.SysFont('Arial',60)
 
 class Balloon():
     def __init__(self,x,y):
-        self.image = 
+        self.image = pygame.image.load('images/balloon.png')
         self.x = x
         self.y = y
         self.screen = screen
 
-    def draw(self):
+    def update(self):
+        balloon1.y = balloon1.y - 1
 
-
+balloon1 = Balloon(500,600)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if balloon1.y > 0:
+            balloon1.update()
+    screen.blit(bday,(0,0))
     text = font1.render('Happy Birthday',True,(0,0,0))
     screen.blit(text,(375,375))
+    screen.blit(balloon1.image,(balloon1.x,balloon1.y))
 
+    pygame.display.update()
