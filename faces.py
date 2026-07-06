9a7e484487dc223219c516aa7e0f857404e899e3
temi134 <temidee2012@gmail.com>
2026-07-06 17:16:41 +0100

    Add files via upload

--- faces.py
+++ faces.py
@@ -1,21 +1,28 @@
 import pygame
+
 pygame.init()
 
 screen = pygame.display.set_mode((250,250))
 
-face = pygame.image.load('images/sadface.png')
+sadface = pygame.image.load("images/sadface.png")
+happyface = pygame.image.load("images/happyface.png")
+
+face = sadface
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            quit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            face = pygame.image.load('images/happyface.png')
+            face = happyface
 
         if event.type == pygame.MOUSEBUTTONUP:
-            face = pygame.image.load('images/sadface.png')
-    screen.fill('white')
-    screen.blit(face,(10,10))
+            face = sadface
+
+    screen.fill("white")
+    screen.blit(face, (10,10))
+
     pygame.display.update()
 
