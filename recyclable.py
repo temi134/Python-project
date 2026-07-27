56fda6714900c2d207471e5e5e78b8c5309d42fc
temi134 <temidee2012@gmail.com>
2026-07-27 17:55:21 +0100

    Add files via upload

--- recyclable.py
+++ recyclable.py
@@ -1,52 +1,113 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((750, 750))
-
+screen = pygame.display.set_mode((800,560))
+background = pygame.image.load("images/nature.jpg")
+score = 0
+lives = 3
+font = pygame.font.SysFont("Arial",20,"bold")
+font1 = pygame.font.SysFont("Arial",100,"bold")
 class Recyclable(pygame.sprite.Sprite):
-    def __init__(self, x, y,images):
+    def __init__(self, x, y, images):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(images)
         self.rect = self.image.get_rect()
-        self.rect.center = [x,y]
+        self.rect.center = [x, y]
         self.screen = screen
 
 class Non_Recyclable(pygame.sprite.Sprite):
-    def __init__(self, x ,y):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("images/non_recyclable.png")
         self.rect = self.image.get_rect()
-        self.rect.center = [x,y]
+        self.rect.center = [x, y]
         self.screen = screen
 
-images = ["images/recyclable.png" , "images/can.jpg"]
+class Bin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/bin.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.screen = screen
+
+
+    def update(self):
+        global score , lives
+        
+        key = pygame.key.get_pressed()
+        if pygame.sprite.spritecollide(self, recyclable_group,True):
+            score = score + 1
+        if pygame.sprite.spritecollide(self, non_recyclable_group,True):
+            lives = lives - 1
+            
+    
+
+        if key[pygame.K_RIGHT]:
+            self.rect.x += 1
+
+        if key[pygame.K_LEFT]:
+            self.rect.x -= 1
+
+        if key[pygame.K_UP]:
+            self.rect.y -= 1
+
+        if key[pygame.K_DOWN]:
+            self.rect.y += 1
+
+images = ["images/recyclable.png", "images/can.png"]
 
 recyclable_group = pygame.sprite.Group()
 for i in range(25):
-    x = random.randint(50, 730)
-    y = random.randint(50, 730)
+    x = random.randint(100, 730)
+    y = random.randint(100, 510)
     image = random.choice(images)
-    recyclable1 = Recyclable(x, y ,image )
+    recyclable1 = Recyclable(x, y, image)
     recyclable_group.add(recyclable1)
 
 non_recyclable_group = pygame.sprite.Group()
 for i in range(10):
-    x = random.randint(50, 730)
-    y = random.randint(50, 730)
-    non_recyclable1 = Non_Recyclable(x,y)
+    x = random.randint(100, 730)
+    y = random.randint(100, 510)
+    non_recyclable1 = Non_Recyclable(x, y)
     non_recyclable_group.add(non_recyclable1)
+print(len(recyclable_group))
+bin1 = Bin(50, 50)
 
 while True:
-    screen.fill("black")
+    screen.blit(background,(0,0))
+    text = font.render("score = "+str (score),True,"black")
+    text2 = font.render("lives ="+str(lives),True,"black")
+    text3 = font1.render("you won",True,"black")
+    text4 = font1.render("you lost",True,"black")
+    screen.blit(text, (10,10))
+    screen.blit(text2,(10,40))
+    if score == 25:
+        screen.blit(text3,(350,250))
+        pygame.display.update()
+        time.sleep(3)
+        break
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
+    if lives <= 0:
+        screen.blit(text4,(350,250))
+        pygame.display.update()
+        time.sleep(3)
+        break
+
     recyclable_group.draw(screen)
     non_recyclable_group.draw(screen)
+
+    bin1.update()
+    screen.blit(bin1.image, bin1.rect)
+    
+
     pygame.display.update()
 
