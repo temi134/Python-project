b591189f3e1c700523a12135f4f7f93c787edcda
temi134 <temidee2012@gmail.com>
2026-08-31 17:56:52 +0100

    Add files via upload

--- spacebattle2.py
+++ spacebattle2.py
@@ -11,6 +11,8 @@ class Spacecraft(pygame.sprite.Sprite):
         self.image = pygame.image.load('images/player.png')
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
+        self.last_shot = pygame.time.get_ticks()
+        self.lives = 3
 
     
 
@@ -18,13 +20,27 @@ class Spacecraft(pygame.sprite.Sprite):
     def update(self):
         key = pygame.key.get_pressed()
 
+
         if key[pygame.K_RIGHT]:
             self.rect.x += 1
 
         if key[pygame.K_LEFT]:
             self.rect.x -= 1
 
-space1 = Spacecraft(400,400)
+        current_time = pygame.time.get_ticks()
+
+        if key[pygame.K_SPACE] and current_time - self.last_shot > 500:
+            bullet1 = Bullet(self.rect.x + 35, self.rect.y)
+            bullet_group1.add(bullet1)
+            self.last_shot  = current_time
+
+        if pygame.sprite.spritecollide(self,bullet_group2,True):
+            self.lives -=  1
+
+
+        
+
+space1 = Spacecraft(400,500)
 
 sprite_group = pygame.sprite.Group()
 sprite_group.add(space1)
@@ -35,6 +51,7 @@ class Spacecraft2(pygame.sprite.Sprite):
         self.image = pygame.image.load('images/player2.png')
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         key = pygame.key.get_pressed()
@@ -45,14 +62,42 @@ class Spacecraft2(pygame.sprite.Sprite):
         if key[pygame.K_a]:
                 self.rect.x -= 1
 
-space2 = Spacecraft2(400,140)
+        current_time = pygame.time.get_ticks()
+
+        if key[pygame.K_s] and current_time - self.last_shot > 500:
+            bullet3 = Bullet2(self.rect.x + 50, self.rect.y)
+            bullet_group2.add(bullet3)
+            self.last_shot = current_time
+
+        
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/player_bullet.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+
+    def update(self):
+        self.rect.y -= 3
+
+class Bullet2(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/red_bullet.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+
+    def update(self):
+        self.rect.y += 3
+     
+
+bullet_group1 = pygame.sprite.Group()
+space2 = Spacecraft2(400,70)
 sprite_group2 = pygame.sprite.Group()
 sprite_group2.add(space2)
-
-
-
-    
-
+bullet_group2 = pygame.sprite.Group()
 
 while True:
     screen.blit(spacebg,(0,0))
@@ -60,6 +105,10 @@ while True:
     sprite_group.update()
     sprite_group2.draw(screen)
     sprite_group2.update()
+    bullet_group1.draw(screen)
+    bullet_group1.update()
+    bullet_group2.draw(screen)
+    bullet_group2.update()
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
