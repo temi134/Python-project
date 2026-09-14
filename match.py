1754af620e0d50967614ca2d88294612c5c0ad31
temi134 <temidee2012@gmail.com>
2026-09-14 18:12:58 +0100

    Add files via upload

--- match.py
+++ match.py
@@ -1,7 +1,10 @@
 import pygame
 
-screen = pygame.display.set_mode((800,800))
+pygame.init()
 
+screen = pygame.display.set_mode((800, 800))
+
+font1 = pygame.font.SysFont('Arial', 30)
 
 mercedes = pygame.image.load('images/mercedes.png')
 nike = pygame.image.load('images/nike.png')
@@ -10,17 +13,47 @@ adidas = pygame.image.load('images/adidas.png')
 microsoft = pygame.image.load('images/microsoft.png')
 audi = pygame.image.load('images/audi.png')
 
+screen.fill('white')
+
 while True:
-    screen.fill('white')
-    screen.blit(mercedes,(650,75))
-    screen.blit(nike,(650,150))
-    screen.blit(microsoft,(650,225))
-    screen.blit(audi,(650,300))
-    screen.blit(google,(650,375))
-    screen.blit(adidas,(650,450))
+    screen.blit(mercedes, (650, 75))
+    screen.blit(nike, (650, 150))
+    screen.blit(microsoft, (650, 225))
+    screen.blit(audi, (650, 300))
+    screen.blit(google, (650, 375))
+    screen.blit(adidas, (650, 450))
+    text = font1.render('Nike', True, (0, 0, 0))
+    screen.blit(text, (50, 450))
+
+    text = font1.render('Mercedes', True, (0, 0, 0))
+    screen.blit(text, (50, 375))
+
+    text = font1.render('Microsoft', True, (0, 0, 0))
+    screen.blit(text, (50, 225))
+
+    text = font1.render('Audi', True, (0, 0, 0))
+    screen.blit(text, (50, 150))
+
+    text = font1.render('Google', True, (0, 0, 0))
+    screen.blit(text, (50, 300))
+
+    text = font1.render('Adidas', True, (0, 0, 0))
+    screen.blit(text, (50, 75))
+
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos2 = pygame.mouse.get_pos()
+            pygame.draw.line(screen,('black'),pos,pos2,5)
+            pygame.display.update()
+
+
         if event.type == pygame.QUIT:
             pygame.quit()
+            quit()
 
     pygame.display.update()
+
     
