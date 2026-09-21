a5b6adb80b7eb488c3f2f09585a3661ba69fd9a7
temi134 <temidee2012@gmail.com>
2026-09-21 18:50:19 +0100

    Add files via upload

--- RPS.py
+++ RPS.py
@@ -4,6 +4,10 @@ import random
 pygame.init()
 
 screen = pygame.display.set_mode((400,400))
+users_score = 0
+computers_score = 0
+users_win = False
+computers_win = False
 
 housebg = pygame.image.load('images/house.jpg')
 housebg = pygame.transform.scale(housebg,(400,400))
@@ -25,19 +29,33 @@ start_time = 0
 
 
 def winner():
-    if users_choice == choice:
-        return 'Draw!'
+    global users_score
+    global computers_score
+    global users_choice
+    global users_win
+    global computers_win
+    if users_choice != '':
+        if users_choice == choice:
+            return 'Draw!'
 
-    if users_choice == 'rock' and choice == 'scissors':
-        return 'You win!'
+        if users_choice == 'rock' and choice == 'scissors':
+            users_win = True
+            return 'You win!'
+            
 
-    if users_choice == 'paper' and choice == 'rock':
-        return 'You win!'
+        if users_choice == 'paper' and choice == 'rock': 
+            users_win = True
+            return 'You win!'
 
-    if users_choice == 'scissors' and choice == 'paper':
-        return 'You win!'
+        if users_choice == 'scissors' and choice == 'paper':
+            users_win = True
+            return 'You win!'
 
+    computers_win = True
     return 'Computer wins!'
+
+
+
 
 
 
@@ -52,7 +70,8 @@ while True:
     screen.blit(font.render('Rock', True, 'white'), (50,180))
     screen.blit(font.render('Scissors', True, 'white'), (180,180))
     screen.blit(font.render('Paper', True, 'white'), (335,180))
-
+    screen.blit(font.render('players score:' + str(users_score),True,'white'),(10,10))
+    screen.blit(font.render('computers score:'+ str(computers_score),True,'white'),(200,10))
     for event in pygame.event.get():
 
         if event.type == pygame.MOUSEBUTTONDOWN and users_choice == '':
@@ -76,9 +95,18 @@ while True:
         text = font.render(winner(), True, 'white')
         screen.blit(text, (150,350))
 
+        
+
         if pygame.time.get_ticks() - start_time >= 3000:
-            users_choice = ''
             choice = random.choice(['rock','paper','scissors'])
+            users_choice = ''
+            if users_win == True:
+                users_score = users_score + 1
+                users_win = False
+            if computers_win == True:
+                computers_score = computers_score + 1
+                computers_win = False
+            
 
     else:
         text = font.render('Choose another one!', True, 'white')
